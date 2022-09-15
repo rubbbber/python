714bb6d3ab0f295f9f55bfea7d2ad1ac1e1cd398
rubbbber <100339034+rubbbber@users.noreply.github.com>
2022-09-15 22:28:01 +0800

    Add files via upload

--- firstcase.py
+++ firstcase.py
@@ -1,12 +1,85 @@
 from distutils.command.install_egg_info import to_filename
+from math import factorial
+from sqlite3 import ProgrammingError
 from secondcase import add,mul
 from multiprocessing import Process,cpu_count
+# import functools
+# import time
 # print(cpu_count())
-#map function countinuing
-store = [("shirt",20.00),
-         ("pants",25.00),
-         ("jacket",50.00),
-         ("socks",10.00)]
+
+#if __name__ == '__main__'
+# 1.Module can be run as a standalone Program
+#   or
+# 2.Module can be imported and used by other Modules
+# python interpreter sets "special variables",one of which is __name__
+# python will assign the __name__ variable a value of '__main__' if it's
+# the initial module being run
+
+#time function
+# time_object = time. localtime() # local time
+# time_object = time. gmtime()# UTC time
+# local_time = time. strftime ("9B 9d %Y %H:9M:9S"', time_object)
+# print (local_time)
+# time_string = "20 April, 2020"
+# time_object = time. strptime (time_ string, "%d %B, %Y")
+# print (time_ object)
+# (year, month, day, hours, minutes, secs, #day of the week, #day of the year, dst)
+# time_tuple = (2020, 4, 20, 4, 20, 0, 0, 0, 0)
+# time_string = time.asctime (time_tuple)
+# time string = time.mktime (time_tuple)
+# print (time_string)
+# print(time_string)
+
+#zip(*iterables) = aggregate elsments from two or more iterables(list,tuples,sets,etc.)
+#                  creates a zip object with paired elements stored in tuples for each elements
+#1
+# usernames = ["Dude","Bro","Mister"]
+# passwords =("password","abc123","guest")
+# users = dict(zip(usernames,passwords))
+# print(type(users))
+# for key,value in users.items():
+#     print(key +":"+ value)
+#2
+# usernames = ["Dude","Bro","Mister"]
+# passwords = ("password","abc123","guest")
+# login_date = ["1/1/2021","1/2/2021","1/3/2021"]
+# users = zip(usernames,passwords,login_date)
+# print(type(users))
+# for i in users:
+#     print(i)
+
+#reduce function
+# 1
+# letters = ["H","E","L","L","O"]
+# word = functools.reduce(lambda x,y:x+y,letters)
+# print(word)
+#2
+# factorial = [1,2,3,4,5]
+# result = functools.reduce(lambda x,y:x*y,factorial)
+# print(result)
+
+#filter function
+# friends = [("Rachel",19),
+#            ("Monica",18),
+#            ("Phoebe",17),
+#            ("Joey",16),
+#            ("Chandler",21),
+#            ("Ross",20)]
+# age = lambda date:date[1]>=18
+# drinking_buddies = list(filter(age,friends))
+# for i in drinking_buddies:
+#     print(i)
+
+#map function
+# store = [("shirt",20.00),
+#          ("pants",25.00),
+#          ("jacket",50.00),
+#          ("socks",10.00)]
+# to_euro = lambda date:date[1]*0.82
+# to_dollar = lambda date:format(date[1]/0.82,".2f")
+# store_dollar = list(map(to_dollar,store))
+# for i in store_dollar:
+#     print(i)
 
 #sort() method = used with lists
 #sort() function = used with iterables
@@ -41,7 +114,7 @@ store = [("shirt",20.00),
 # multiply = lambda x,y,:x*y
 # add = lambda x,y,z:x+y+z
 # full_name = lambda first_name,last_name:first_name+""+last_name
-# age_check =lambda age : True if  age >=18 else False
+# age_check = lambda age : True if  age >=18 else False
 # print(age_check(18))
 # print(double(5))
 
@@ -79,6 +152,10 @@ store = [("shirt",20.00),
 # print()
 
 #dictionary comprehension
+#dictionary = {key:expression for (key:value) in iterable}
+#dictionary = {key: expression for (key:value) in iterable if conditional}
+#dictionary = {key: (if/else) for (key:value) in iterable}
+#dictionary = {key: function(value) for (key,value) in iterable}
 #1
 # cities_in_F = {"New York":32,"Boston":75,"Los Angeles":100,"Chiago":50}
 # cities_in_C = {key:format((value-32)*(5/9),'.2f') for (key,value) in cities_in_F.items()}
